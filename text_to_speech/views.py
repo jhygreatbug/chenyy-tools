6f0cd5e4c3d9ac1495e2fbc7c245a747dfcc1d3c
chenyy <jhygreatbug@gmail.com>
2017-09-23 10:33:44 +0000

    Update: 语音合成接口，还差调用动态链接库的问题

--- text_to_speech/views.py
+++ text_to_speech/views.py
@@ -4,16 +4,39 @@ from __future__ import unicode_literals
 from django.shortcuts import render_to_response
 from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+import ctypes
+import time
+import os
+
+TTS_LIB_NAME = 'libmsc.so'
+TTS_LIB_PATH = os.path.join(*(os.path.split(__file__)[:-1] + (TTS_LIB_NAME,)))
+tts = ctypes.cdll.LoadLibrary(TTS_LIB_PATH)
+
+MSP_SUCCESS = 0
+TTS_LOGIN_PARAMS = 'appid = 59bd4d08, work_dir = .'
+
+TTS_SESSION_BEGIN_PARAMS = 'engine_type = local,voice_name=xiaoyan, text_encoding = UTF8, tts_res_path = fo|res/tts/xiaoyan.jet;fo|res/tts/common.jet, sample_rate = 16000, speed = 50, volume = 50, pitch = 50, rdn = 2'
 
 def view(request):
 	return render_to_response('tool/text_to_speech.html')
 
 def tts(request):
+
 	text = '';
 	if 'text' not in request.GET:
 		return HttpResponse(status = 403)
 	else:
 		text = request.GET['text']
-	src = 'wav';
-	return JsonResponse({ 'src': src })
+
+	result = tts.MSPLogin(None, None, TTS_LOGIN_PARAMS)
+	if result != MSP_SUCCESS:
+		return HttpResponse(status = 500)
+
+	filename = str(time.time()) + '.wav'
+	result = tts.text_to_speech(text, filename, TTS_SESSION_BEGIN_PARAMS);
+	if result != MSP_SUCCESS:
+		return HttpResponse(status = 500)
+
+	tts.MSPLogout();
+
+	return JsonResponse({ 'src': filename })
