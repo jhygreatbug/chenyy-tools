6fe8b3e1314fcb05964ba10a5bc6de398d900e67
chenyy <jhygreatbug@gmail.com>
2017-09-29 09:44:48 +0000

    Update: 语音合成基本完成，剩下讯飞老返回空音频的问题

--- text_to_speech/views.py
+++ text_to_speech/views.py
@@ -1,21 +1,76 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.shortcuts import render_to_response
 from django.http import JsonResponse, HttpResponse
+from io import BytesIO
 
 import ctypes
 import time
 import os
+import wave
 
-TTS_LIB_NAME = 'libmsc.so'
+TTS_LIB_NAME = 'libs/x64/libmsc.so'
 TTS_LIB_PATH = os.path.join(*(os.path.split(__file__)[:-1] + (TTS_LIB_NAME,)))
-tts = ctypes.cdll.LoadLibrary(TTS_LIB_PATH)
+msc = ctypes.cdll.LoadLibrary(TTS_LIB_PATH)
 
 MSP_SUCCESS = 0
-TTS_LOGIN_PARAMS = 'appid = 59bd4d08, work_dir = .'
+TTS_LOGIN_PARAMS = b'appid = 59bd4d08, work_dir = .'
 
-TTS_SESSION_BEGIN_PARAMS = 'engine_type = local,voice_name=xiaoyan, text_encoding = UTF8, tts_res_path = fo|res/tts/xiaoyan.jet;fo|res/tts/common.jet, sample_rate = 16000, speed = 50, volume = 50, pitch = 50, rdn = 2'
+TTS_SESSION_BEGIN_PARAMS = 'voice_name = xiaoyan, text_encoding = utf8, sample_rate = 16000, speed = 50, volume = 50, pitch = 50, rdn = 2'
+
+
+def saveWave(raw_data, _tmpFile = 'test.wav'):
+	f = wave.open('/var/www/media/' + _tmpFile, 'w')
+	f.setparams((1, 2, 16000, 262720, 'NONE', 'not compressed'))
+	f.writeframesraw(raw_data)
+	f.close()
+	return _tmpFile
+
+def text_to_speech(text, filename):
+	text = b'测试文本'
+	result_c = ctypes.c_int()
+	result = 0
+
+	session = msc.QTTSSessionBegin(TTS_SESSION_BEGIN_PARAMS, ctypes.byref(result_c))
+	print session
+	if result_c.value != MSP_SUCCESS:
+		return '%d: QTTSSessionBegin failed' % result_c.value
+
+	result = msc.QTTSTextPut(session, text, len(text), None)
+	print text
+	if result != MSP_SUCCESS:
+		msc.QTTSSessionEnd(session, 'TextPutError');
+		return '%d: QTTSTextPut failed' % result
+
+	audio_len = ctypes.c_uint(0)
+	synth_status = ctypes.c_int(0)
+	f = BytesIO()
+	while True:
+		p = msc.QTTSAudioGet(session, ctypes.byref(audio_len), ctypes.byref(synth_status), ctypes.byref(result_c))
+		print p
+		print audio_len.value
+
+		if result_c.value != MSP_SUCCESS:
+			f.close()
+			msc.QTTSSessionEnd(session, 'TextPutError');
+			return '%d: QTTSAudioGet failed' % result_c.value
+
+		if p != None:
+			buf = (ctypes.c_char * audio_len.value).from_address(p)
+			f.write(buf)
+
+		if synth_status.value != 2:
+			saveWave(f.getvalue(), filename)
+			break
+
+		time.sleep(0.1)
+
+	f.close()
+	msc.QTTSSessionEnd(session, 'TextPutError');
+
+	return 0
 
 def view(request):
 	return render_to_response('tool/text_to_speech.html')
@@ -28,15 +83,15 @@ def tts(request):
 	else:
 		text = request.GET['text']
 
-	result = tts.MSPLogin(None, None, TTS_LOGIN_PARAMS)
+	result = msc.MSPLogin(None, None, TTS_LOGIN_PARAMS)
 	if result != MSP_SUCCESS:
 		return HttpResponse(status = 500)
 
-	filename = str(time.time()) + '.wav'
-	result = tts.text_to_speech(text, filename, TTS_SESSION_BEGIN_PARAMS);
-	if result != MSP_SUCCESS:
-		return HttpResponse(status = 500)
+	filename = str(int(time.time())) + '.wav'
+	result = text_to_speech(text, filename);
+	if result != 0:
+		return HttpResponse(status = 500, content = result)
 
-	tts.MSPLogout();
+	msc.MSPLogout();
 
 	return JsonResponse({ 'src': filename })
